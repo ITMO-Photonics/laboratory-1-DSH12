ff2b43a1811a1f6f71c3e729251e0e6015c6c0f4
DSH12 <shara-misha@yandex.ru>
2018-03-17 12:11:10 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,17 +2,27 @@ import scipy
 from scipy import optimize
 import numpy
 
+import time
+
 def f(x):
 	return (numpy.cos(x))/(1.+x*x)
 
 def f1(x):
 	return (-(numpy.sin(x))/(1.+x*x))-((2.*x*numpy.cos(x))/((1.+x*x)*(1.+x*x)))
 	
-sol = optimize.bisect(f, 0.1, 2.4)
-print("bisect", sol)
-sol = optimize.newton(f, 1., f1)
-print("newton and f1", sol)
-sol = optimize.newton(f, 1.5)
-print("newton", sol)
-sol = optimize.brentq(f, 0.1, 2.4)
-print("brentq", sol)
+t = time.process_time()
+for i in range (10000):
+	sol = optimize.bisect(f, 0.1, 2.4)
+print("bisect result =", sol, "t =", (time.process_time()-t))
+t = time.process_time()
+for i in range (10000):
+	sol = optimize.newton(f, 1., f1)
+print("newton and f1 result =", sol, "t =", (time.process_time()-t))
+t = time.process_time()
+for i in range (10000):
+	sol = optimize.newton(f, 1.5)
+print("newton result =", sol, "t =", (time.process_time()-t))
+t = time.process_time()
+for i in range (10000):
+	sol = optimize.brentq(f, 0.1, 2.4)
+print("brentq result =", sol, "t =", (time.process_time()-t))
