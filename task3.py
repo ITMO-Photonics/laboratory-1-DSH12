2a713b5b435c59dab562e0484a3ac9f87fb4df1a
DSH12 <shara-misha@yandex.ru>
2018-03-17 12:47:04 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,18 @@
+import scipy
+from scipy import linalg
+import numpy
 
+A = numpy.zeros((101,101))
+i,j = numpy.indices(A.shape)
+for k in range (3):
+	A[i==j+k]=1
+	
+B = numpy.zeros((101))
+for k in range (101):
+	B[k]=k
+
+print (A)
+print (B)
+
+x = linalg.solve(A, B)
+print(x)
